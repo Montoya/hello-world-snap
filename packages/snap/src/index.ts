2c8735e78a7b680b5e2f25c30bbe7fa62f8141f1
Christian Montoya <christian.montoya@consensys.net>
2024-04-30 20:29:18 -0400

    Complete
    Simple, stateless demo of interactive UI

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,5 +1,5 @@
-import type { OnHomePageHandler } from '@metamask/snaps-sdk';
-import { panel, text, button, form, input } from '@metamask/snaps-sdk';
+import type { OnHomePageHandler, OnUserInputHandler } from '@metamask/snaps-sdk';
+import { panel, text, spinner, button, form, input } from '@metamask/snaps-sdk';
 
 /**
  * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
@@ -12,30 +12,41 @@ import { panel, text, button, form, input } from '@metamask/snaps-sdk';
  * @throws If the request method is not valid for this snap.
  */
 export const onHomePage: OnHomePageHandler = async () => {
+  // we create an interface 
+  const interfaceId = await snap.request({
+    method: "snap_createInterface",
+    params: {
+      ui: panel([
+        spinner()
+      ])
+    },
+  });
+  snap.request({
+    method: "snap_updateInterface",
+    params: {
+      id: interfaceId,
+      ui: panel([
+        button({ 
+          value: "Say hello",
+          name: `btn-${interfaceId}`,
+        }),
+      ]),
+    },
+  });
   return { 
-    content: panel([
-      text("Hello, world!"),
-      button({ 
-        value: "Click Me",
-        name: "Interactive Button"
-      }),
-      input({
-        name: "Interactive Input",
-        label: "Fill Me"
-      }),
-      form({
-        name: "form-to-fill",
-        children: [
-          input({
-            name: "user-name",
-            placeholder: "Your name",
-          }),
-          button({
-            value: "Submit",
-            buttonType: "submit",
-          }),
-        ]
-      }),
-    ]),
-  }; 
+    id: interfaceId
+  }
 };
+
+export const onUserInput: OnUserInputHandler = async ({id, event}) => { 
+  const interfaceId = id.replace('btn-',''); 
+  await snap.request({
+    method: "snap_updateInterface", 
+    params: { 
+      id: interfaceId,
+      ui: panel([
+        text("Hello, world!"),
+      ]),
+    },
+  }); 
+}; 
